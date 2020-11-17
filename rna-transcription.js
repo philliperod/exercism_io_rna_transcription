b2cd367081ac5e11e079d5055d6abef3b8173fcd
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-17 15:45:51 -0600

    created an object to correspond with matching letters needed; created a function that accepts an input which will be put into an array and stored into a variable; use a conditional to check if it is an empty string and return an empty string; if not, iterate over the array, return the matching value from the prop, then join it into a string

--- rna-transcription.js
+++ rna-transcription.js
@@ -1,11 +1,24 @@
-const sequenceDNA = ['A', 'C', 'G', 'T'];
-const sequenceRNA = ['A', 'C', 'G', 'U'];
+const transcription = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U',
+};
 
 export const toRna = (nucleotide) => {
-  if (sequenceDNA[nucleotide] === '') {
-    return '';
-  }
+  const sequenceDNA = nucleotide.split('');
 
-  return sequenceDNA[nucleotide] === sequenceRNA[]
-
+  let sequenceRNA =
+    nucleotide === ''
+      ? ''
+      : sequenceDNA.map((strand) => transcription[strand]).join('');
+  return sequenceRNA;
 };
+
+// first, we want to transcribe or match the corresponding DNA letter to the RNA letter and put it was an object
+// want to create a parameter for the function to accept input(s)
+// then that those input(s) and put it into an array and store it into a variable
+// create a conditional using ternary operator
+// if parameter is an empty string then return an empty string
+// if received an array with value(s) then create a new array with map()
+// iterate over each value in the array, look for the name/value pair in the transcription object, and join it into a string with no spaces
